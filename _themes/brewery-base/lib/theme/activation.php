68a97a3ca76990f3ebe77cd7029f50de7e4c81a7
Tyler Young <tyler@x-plane.com>
2015-03-21 19:11:58 -0500

    Changed default home page

--- _themes/brewery-base/lib/theme/activation.php
+++ _themes/brewery-base/lib/theme/activation.php
@@ -161,9 +161,63 @@ function roots_theme_activation_action()
         $pages_to_create = array_diff($default_pages, $temp);
 
         foreach ($pages_to_create as $new_page_title) {
+            $home = <<<EOL
+<h2>[bg highlight]By beer lovers, for beer lovers.[/bg]</h2>
+
+[bg mb=10]From effervescent, Belgian-style saisons to the most massive, monstrous stouts...[/bg]
+
+[bg mb=10]From cutting-edge, label-defying experimental brews to ancient recipe revivals...[/bg]
+
+[bg mb=10]Forever Young stands for loving beer &amp; beer culture to the fullest.[/bg]
+
+&nbsp;
+
+&nbsp;
+
+<h2>[bg highlight]Our Beers[/bg]</h2>
+
+[beers columns=6 length=0 /]
+
+&nbsp;
+
+&nbsp;
+
+<h2>[bg highlight]Our Story[/bg]</h2>
+
+[container]
+
+Here at Forever Young, we're brewers and culinarians.
+
+Our mission is simple: to produce fresh, flavorful beers using the finest local ingredients and the best of both old &amp; new brewing techniques.
+
+We want our beer to taste good, but we also want it to taste good with food. We don’t want to blow-out your palate, we want you to explore the hidden flavors and understand the complexities of the taste.
+
+We're a crazy, kooky bunch, but we work hard &amp; get the job done. At work, we try to keep our day-to-day activities light &amp; fun.
+
+Above all, we’re adventurous. We started as homebrewers, and we've never lost that sense of exploration. We hope you'll join us on our beer adventure.
+
+[/container]
+
+&nbsp;
+
+&nbsp;
+
+<h2>[bg highlight]Visit Our Taproom[/bg]</h2>
+
+[widgets_on_pages]
+
+<div class="text-center"><a class="btn btn-primary btn-lg " href="/tour/">View Our Tour Schedule</a></div>
+
+&nbsp;
+
+&nbsp;
+EOL;
+
+
+
             $add_default_pages = array(
                 'post_title' => $new_page_title,
-                'post_content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, orci ac laoreet cursus, dolor sem luctus lorem, eget consequat magna felis a magna. Aliquam scelerisque condimentum ante, eget facilisis tortor lobortis in. In interdum venenatis justo eget consequat. Morbi commodo rhoncus mi nec pharetra. Aliquam erat volutpat. Mauris non lorem eu dolor hendrerit dapibus. Mauris mollis nisl quis sapien posuere consectetur. Nullam in sapien at nisi ornare bibendum at ut lectus. Pellentesque ut magna mauris. Nam viverra suscipit ligula, sed accumsan enim placerat nec. Cras vitae metus vel dolor ultrices sagittis. Duis venenatis augue sed risus laoreet congue ac ac leo. Donec fermentum accumsan libero sit amet iaculis. Duis tristique dictum enim, ac fringilla risus bibendum in. Nunc ornare, quam sit amet ultricies gravida, tortor mi malesuada urna, quis commodo dui nibh in lacus. Nunc vel tortor mi. Pellentesque vel urna a arcu adipiscing imperdiet vitae sit amet neque. Integer eu lectus et nunc dictum sagittis. Curabitur commodo vulputate fringilla. Sed eleifend, arcu convallis adipiscing congue, dui turpis commodo magna, et vehicula sapien turpis sit amet nisi.',
+                'post_content' => $home,
                 'post_status' => 'publish',
                 'post_type' => 'page'
             );
