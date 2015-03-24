122a367079b07703379e3db4e3823067950ea892
Tyler Young <tyler@x-plane.com>
2015-03-24 07:17:30 -0500

    Minor fix for navbar branding in mobile layout

--- _themes/brewery-base/lib/scripts.php
+++ _themes/brewery-base/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts()
 {
-    wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6fc10955b5371eae85cd422e1891106b');
+    wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ac0f39b9b49b7bcef1f035747e9b0883');
 
     // jQuery is loaded using the same method from HTML5 Boilerplate:
     // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
