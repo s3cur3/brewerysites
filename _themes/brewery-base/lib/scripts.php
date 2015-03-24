47b469be2dc564664584597268f4ac8b21855e0e
Tyler Young <tyler@x-plane.com>
2015-03-24 08:51:28 -0500

    More fixes for mobile layouts

--- _themes/brewery-base/lib/scripts.php
+++ _themes/brewery-base/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts()
 {
-    wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ac0f39b9b49b7bcef1f035747e9b0883');
+    wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '11f6e168ca4245d401c169b0c882209e');
 
     // jQuery is loaded using the same method from HTML5 Boilerplate:
     // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
